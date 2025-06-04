59054af4d7fe608f45e5435cfa4eb8d244d05ba0
Nafiz Karim <nafiz.rim@gmail.com>
2025-06-04 19:01:56 -0400

    created the errors folder, started writing the stream_string function
    the stream string function will generate bytes using the stream_bytes function and yield the string version of the chunks

--- oral_history_repository_project/models/corpus.py
+++ oral_history_repository_project/models/corpus.py
@@ -1,3 +1,4 @@
+from util import stream
 class Corpus:
     def __init__(self,id:int, filename:str , relative_path:str):
         self.id = id 
@@ -8,4 +9,5 @@ class Corpus:
     def get_corpus(self, filename:str, relative_path:str):
         # internally call the utility fucntion 
         # return the bytes in the format this corpus object should return
-        pass
+        # return the generator expression 
+        return stream.stream_bytes(filename , relative_path)
