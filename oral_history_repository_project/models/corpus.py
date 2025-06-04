9af573914144efc66492586a7578ea5c49350693
Nafiz Karim <nafiz.rim@gmail.com>
2025-06-04 07:44:08 -0400

    added the id instance variable in the Corpus class

---
+++ oral_history_repository_project/models/corpus.py
@@ -0,0 +1,11 @@
+class Corpus:
+    def __init__(self,id:int, filename:str , relative_path:str):
+        self.id = id 
+        self._filename = filename
+        self._relative_path = relative_path
+    
+
+    def get_corpus(self, filename:str, relative_path:str):
+        # internally call the utility fucntion 
+        # return the bytes in the format this corpus object should return
+        pass
